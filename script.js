c696300eb5d35a5a6ebb100e67468093c3ce6b42
subham-20000901 <sinhasubh2000@gmail.com>
2026-02-13 20:11:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 //your JS code here. If required.
+let addBtn = document.getElementById("submit");
+addBtn.addEventListener("click",function(e){
+	 e.preventDefault(); 
+	let title = document.getElementById("title").value;
+	let author = document.getElementById("author").value;
+	let isbn = document.getElementById("isbn").value;
+
+	let booklist = document.getElementById("book-list");
+	let row = document.createElement("tr");
+	row.innerHTML = 
+		`<td>${title}</td>
+	     <td>${author}</td>
+		 <td>${isbn}</td>
+		 <td><button class="deleteBtn">X</button></td>`;
+	
+	 booklist.appendChild(row);
+
+    document.getElementById("title").value = "";
+    document.getElementById("author").value = "";
+    document.getElementById("isbn").value = "";
+})
+
+document.getElementById("book-list").addEventListener("click",function(e){
+	if(e.target.classList.contains("deleteBtn")) {
+		e.target.parentElement.parentElement.remove();
+	}
+})
